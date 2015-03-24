ddf66dbcbd2b0a8965f7afab8bcb68eb7c9ff8d1
QHShao <qhapp.shao@gmail.com>
2015-03-24 14:04:04 -0500

    all

--- Project/include/connection.php
+++ Project/include/connection.php
@@ -1,9 +1,9 @@
 <?php
 	$dbhost = 'localhost';
-	$dbuser = 'root';
-	$dbpass = '';
-	$db = 'Account';
+	$dbuser = 'CS411';
+	$dbpass = 'q1w2e3';
+	$db_name = 'Account';
 
-	$connection = mysql_connect($dbhost, $dbuser, $dbpass);
-	mysql_select_db($db);
+	$connection = mysql_connect($dbhost, $dbuser, $dbpass) or die();
+	mysql_select_db($db_name) or die();
 ?>
--- Project/signup/create.php
+++ Project/signup/create.php
@@ -1,12 +1,44 @@
+<head>
+	<title>You have registered successfully!</title>
+	<link rel="stylesheet" href="normalize.css">
+	<link rel="stylesheet" href="style.css">
+	<link rel="apple-touch-icon" sizes="57x57" href="../icon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="../icon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="../icon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="../icon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="../icon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="../icon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="../icon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="../icon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="../icon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="../icon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="../icon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="../icon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="../icon/favicon-16x16.png">
+	<link rel="manifest" href="../icon/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="../icon/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+</head>
+
+
+
 <?php
 	include '../include/connection.php';
-	$uname = $_POST('username');
-	$email = $_POST('useremail');
-	$password = $_POST('password');
+	$uname = $_POST['username'];
+	//echo $uname;
+	$email = $_POST['useremail'];
+	//echo $email;
+	$password = $_POST['password'];
+	//echo $password;
+	
+	$submit = $_POST['Submit'];
+	//echo $submit;
 
-	if($_POST('submit')){
-		mysql_query = "INSERT INTO User(`UIUCEmail`, `Name`, `password`, `Admin`)
-						VALUES('$email', '$uname', '$password', 0) "or die(mysql_error());//if admin use 1
+	if($submit){
+		mysql_query("INSERT INTO User(`UIUCEmail`, `Name`, `password`, `Admin`)
+						VALUES('$email', '$uname', '$password', 0)") or die(mysql_error());//if admin use 1
+		echo "You have registered successfully!";
 	}
-	echo "You have registered successfully!"
+	
 ?>
